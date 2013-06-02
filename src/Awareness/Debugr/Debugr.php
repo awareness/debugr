32cb1d255a8d937456897d015e86433cf34b129d
Colby Rabideau <idontknow5691@gmail.com>
2013-06-01 21:47:36 -0400

    comments!

--- src/Awareness/Debugr/Debugr.php
+++ src/Awareness/Debugr/Debugr.php
@@ -2,6 +2,12 @@
 
 class Debugr {
 
+  /**
+   * print_r in <pre> tags
+   *
+   * @param  mixed
+   * @param  bool   if true, exit
+   */
   public static function dump($data, $exit=true)
   {
     echo '<pre>';
